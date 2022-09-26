e6592d4949107c59dccadb90c27282e81371aa65
Hicham Allalou <ph.allalouh@gmail.com>
2022-09-26 23:18:12 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+
+  </header>
+
+  <!-- <RouterView /> -->
+</template>
